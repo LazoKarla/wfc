f42d966430f4bf8e1dd056d508b53dc6bc21234c
Karla Lazo <karlalazoot@gmail.com>
2024-11-16 18:30:20 -0500

    ingreso y estructura de datos

---
+++ sketch.js
@@ -0,0 +1,79 @@
+const celdas = []; // 4x4
+const RETICULA = 4;
+
+const azulejos = [];
+const NA = 6; // número de azulejos
+const reglas = [
+  // reglas de los bordes de cada azulejo
+  {
+    //tile 0
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 0,
+  },
+
+  {
+    //tile 1
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 0,
+  },
+
+  {
+    //tile 2
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 1,
+  },
+
+  {
+    //tile 3
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 1,
+  },
+
+  {
+    //tile 4
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 1,
+  },
+
+  {
+    //tile 5
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 1,
+  },
+];
+
+function preload() {
+  for (let i = 0; i < NA; i++) {
+    azulejos[i] = loadImage(`azulejos/tile${i}.png`);
+  }
+}
+
+function setup() {
+  createCanvas(1080, 1080);
+
+  let opcionesI = [];
+  for (let i = 0; i < azulejos.length; i++) {
+    opcionesI.push(i);
+  }
+  for (let i = 0; i < RETICULA * RETICULA; i++) {
+    celdas[i] = {
+      colapsada: false,
+      opciones: opcionesI,
+    };
+  }
+  print(celdas);
+}
+
+function draw() {}
