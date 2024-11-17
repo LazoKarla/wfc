c5ddc0bf71f85404a02e25b2b11926afa7c169e3
Karla Lazo <karlalazoot@gmail.com>
2024-11-16 20:37:38 -0500

    celdas colapsadas

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 const celdas = []; // 4x4
 const RETICULA = 4;
+let ancho; //altura de celda
+let alto; //anchura de celda
 
 const azulejos = [];
-const NA = 6; // número de azulejos
+const NA = 5; // número de azulejos
 const reglas = [
   // reglas de los bordes de cada azulejo
   {
@@ -63,6 +65,9 @@ function preload() {
 function setup() {
   createCanvas(1080, 1080);
 
+  ancho = width / RETICULA;
+  alto = height / RETICULA;
+
   let opcionesI = [];
   for (let i = 0; i < azulejos.length; i++) {
     opcionesI.push(i);
@@ -73,7 +78,52 @@ function setup() {
       opciones: opcionesI,
     };
   }
-  print(celdas);
+  // celdas[8].colapsada = true;
+  // celdas[3].colapsada = true;
+  // celdas[11].opciones = [5,6,8];
+  //celdas[4].opciones = [4,7,11];
+  //celdas[1].opciones = [6,4,8,10];
+  //celdas[5].opciones = [11,6, 4, 8, 10];
 }
 
-function draw() {}
+function draw() {
+  //background(111);
+  const celdasDisponibles = celdas.filter((celda) => {
+    return celda.colapsada == false;
+  });
+  if (celdasDisponibles.length > 0) {
+    celdasDisponibles.sort((a, b) => {
+      return a.opciones.length - b.opciones.length;
+    });
+
+    const celdasPorColapsar = celdasDisponibles.filter((celda) => {
+      return celda.opciones.length == celdasDisponibles[0].opciones.length;
+    });
+
+    const celdaSeleccionada = random(celdasPorColapsar);
+    celdaSeleccionada.colapsada = true;
+
+    const opcionSeleccionada = random(celdaSeleccionada.opciones);
+
+    celdaSeleccionada.opciones = [opcionSeleccionada];
+
+    print(celdaSeleccionada);
+
+    for (let x = 0; x < RETICULA; x++) {
+      for (let y = 0; y < RETICULA; y++) {
+        const celdaIndex = x + y * RETICULA;
+        const celdaActual = celdas[celdaIndex];
+        if (celdaActual.colapsada) {
+          image(
+            azulejos[celdaActual.opciones[0]],
+            x * ancho,
+            y * alto,
+            ancho,
+            alto
+          );
+        }
+      }
+    }
+  }
+  //noLoop();
+}
